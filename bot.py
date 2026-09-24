534733a68dde42a62a1e64e47a6de79762a7eb42
popakakapipi002-ctrl <popakakapipi002@gmail.com>
2026-09-24 16:05:28 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,43 @@
+import os
+import telebot
+from flask import Flask, request
+
+BOT_TOKEN = "8686197936:AAGPQ3_aDzzWiplC48bbKXuwYl8OO-fPeo0"
+bot = telebot.TeleBot(BOT_TOKEN)
+app = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я бот с расписанием АГПК.")
+
+@bot.message_handler(commands=['day'])
+def day(message):
+    bot.send_message(message.chat.id, "Расписание на день (пока пусто)")
+
+@bot.message_handler(commands=['week'])
+def week(message):
+    bot.send_message(message.chat.id, "Расписание на неделю (пока пусто)")
+
+@app.route('/' + BOT_TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+@app.route('/health')
+def health():
+    return "OK", 200ss
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url="https://НАЗВАНИЕ_ТВОЕГО_СЕРВИСА.onrender.com/" + BOT_TOKEN)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+app.run - Данный веб-сайт выставлен на продажу! - app Ресурсы и информация.
+app.run
+
+
+
+
+
